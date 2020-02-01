50b444de40e5457712e5592dde675761fd44cd84
Dionis Ramadani <54983399+jirn073-76@users.noreply.github.com>
2020-02-01 11:09:18 +0100

    Create TimeFormatter.cs

---
+++ src/kyu4/TimeFormatter.cs
@@ -0,0 +1,44 @@
+// CSharp
+//Not very proud of this one either :/
+
+using System;
+
+public class HumanTimeFormat{
+  public static String formatDuration(int seconds) {
+    if(seconds == 0)
+      return "now";
+    
+    int[] date= {seconds / (60*60*24*365),
+                 seconds % (60*60*24*365) / (60*60*24),
+                 seconds % (60*60*24) / (60*60),
+                 seconds % (60*60) / 60,
+                 seconds % 60};
+    
+    string str = "";
+    for(int i = 0; i < 5; i++) {
+      
+      string currentChrono = i == 0 ? "year" :
+                             i == 1 ? "day" :
+                             i == 2 ? "hour" : 
+                             i == 3 ? "minute" : 
+                             i == 4 ? "second" : "";
+      
+      if(date[i] > 0) {
+        str += date[i] + " " + currentChrono + (date[i] > 1 ? "s" : "");
+        
+        if((i == 2 && (date[i+1] != 0 && date[i+2] != 0))||
+           (i == 1 && ((date[i+1] != 0 && date[i+2] != 0)||(date[i+1] != 0 && date[i+3] != 0)||(date[i+2] != 0 && date[i+3] != 0)))||
+           (i == 0 && ((date[i+1] != 0 && date[i+2] != 0)||(date[i+1] != 0 && date[i+3] != 0)||(date[i+1] != 0 && date[i+4] != 0)||(date[i+2] != 0 && date[i+3] != 0)||(date[i+2] != 0 && date[i+4] != 0)||(date[i+3] != 0 && date[i+4] != 0))))
+            str += ", ";
+
+        if((i == 3 && date[i+1] != 0)||
+           (i == 2 && ((date[i+1] == 0 && date[i+2] != 0)||(date[i+1] != 0 && date[i+2] == 0)))||
+           (i == 1 && ((date[i+1] != 0 && date[i+2] == 0 && date[i+3] == 0)||(date[i+1] == 0 && date[i+2] != 0 && date[i+3] == 0)||(date[i+1] == 0 && date[i+2] == 0 && date[i+3] != 0)))||
+           (i == 0 && ((date[i+1] != 0 && date[i+2] == 0 && date[i+3] == 0 && date[i+4] == 0)||(date[i+1] == 0 && date[i+2] != 0 && date[i+3] == 0 && date[i+4] == 0)||(date[i+1] == 0 && date[i+2] == 0 && date[i+3] != 0 && date[i+4] == 0)||(date[i+1] == 0 && date[i+2] == 0 && date[i+3] == 0 && date[i+4] != 0))))
+            str += " and ";
+      }    
+    }
+    
+    return str;
+  }
+}
