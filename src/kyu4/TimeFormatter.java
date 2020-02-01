76c8b6cefba037b6824e9c4ed057a7f46ccfdc9f
Dionis Ramadani <54983399+jirn073-76@users.noreply.github.com>
2020-02-01 11:08:32 +0100

    Update TimeFormatter.java

--- src/kyu4/TimeFormatter.java
+++ src/kyu4/TimeFormatter.java
@@ -1,20 +1,42 @@
-import java.util.Arrays;
-import java.util.stream.*;
-public class TimeFormatter {    
-    public static String formatDuration(int seconds) {            
-        return seconds == 0 ? "now" : 
-                Arrays.stream(
-                  new String[]{
-                       formatTime("year",  (seconds / 31536000)),
-                       formatTime("day",   (seconds / 86400)%365),
-                       formatTime("hour",  (seconds / 3600)%24),
-                       formatTime("minute",(seconds / 60)%60),
-                       formatTime("second",(seconds%3600)%60)})
-                      .filter(e->e!="")
-                      .collect(Collectors.joining(", "))
-                      .replaceAll(", (?!.+,)", " and ");
+// Java
+
+public class TimeFormatter {
+  public static String formatDuration(int seconds) {
+    if(seconds == 0)
+      return "now";
+    
+    int[] date= {seconds / (60*60*24*365),
+           seconds % (60*60*24*365) / (60*60*24),
+           seconds % (60*60*24) / (60*60),
+           seconds % (60*60) / 60,
+           seconds % 60};
+    
+    String str = "";
+    
+    for(int i = 0; i < 5; i++) {
+      
+      String currentChrono = i == 0 ? "year" :
+                   i == 1 ? "day" :
+                   i == 2 ? "hour" : 
+                   i == 3 ? "minute" : 
+                   i == 4 ? "second" : "";
+      
+      if(date[i] > 0) {
+        str += date[i] + " " + currentChrono + (date[i] > 1 ? "s" : "");
+        
+        if((i == 2 && (date[i+1] != 0 && date[i+2] != 0))||
+           (i == 1 && ((date[i+1] != 0 && date[i+2] != 0)||(date[i+1] != 0 && date[i+3] != 0)||(date[i+2] != 0 && date[i+3] != 0)))||
+           (i == 0 && ((date[i+1] != 0 && date[i+2] != 0)||(date[i+1] != 0 && date[i+3] != 0)||(date[i+1] != 0 && date[i+4] != 0)||(date[i+2] != 0 && date[i+3] != 0)||(date[i+2] != 0 && date[i+4] != 0)||(date[i+3] != 0 && date[i+4] != 0))))
+            str += ", ";
+
+        if((i == 3 && date[i+1] != 0)||
+           (i == 2 && ((date[i+1] == 0 && date[i+2] != 0)||(date[i+1] != 0 && date[i+2] == 0)))||
+           (i == 1 && ((date[i+1] != 0 && date[i+2] == 0 && date[i+3] == 0)||(date[i+1] == 0 && date[i+2] != 0 && date[i+3] == 0)||(date[i+1] == 0 && date[i+2] == 0 && date[i+3] != 0)))||
+           (i == 0 && ((date[i+1] != 0 && date[i+2] == 0 && date[i+3] == 0 && date[i+4] == 0)||(date[i+1] == 0 && date[i+2] != 0 && date[i+3] == 0 && date[i+4] == 0)||(date[i+1] == 0 && date[i+2] == 0 && date[i+3] != 0 && date[i+4] == 0)||(date[i+1] == 0 && date[i+2] == 0 && date[i+3] == 0 && date[i+4] != 0))))
+            str += " and ";
+      }    
     }
-    public static String formatTime(String s, int time){
-      return time==0 ? "" : Integer.toString(time)+ " " + s + (time==1?"" : "s");
-    }
+    
+    return str;
+  }
 }
