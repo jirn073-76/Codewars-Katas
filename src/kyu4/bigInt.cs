856e313b68860e4f047fe15411a7f427b261d043
Dionis Ramadani <54983399+jirn073-76@users.noreply.github.com>
2020-02-05 19:13:27 +0100

    Create bigInt.cs

---
+++ src/kyu4/bigInt.cs
@@ -0,0 +1,11 @@
+// CSharp
+using System;
+using System.Numerics;
+
+public class Kata
+{
+  public static string Add(string a, string b)
+  {
+    return (BigInteger.Parse(a) + BigInteger.Parse(b)).ToString();
+  }
+}
